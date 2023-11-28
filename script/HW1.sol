d9ec39a79418055a15eafd9bd0f34f233b50a2c9
Edward <edward821220@gmail.com>
2023-11-28 12:45:14 +0800

    add necessary contract files.

--- script/HW1.sol
+++ script/HW1.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "../contracts/CErc20Delegator.sol";
 
-contract CounterScript is Script {
+contract HW1Script is Script {
     function setUp() public {}
 
     function run() public {
