9a1a1e571b398eeb8456f87b31b2eb0da2963988
Edward <edward821220@gmail.com>
2023-11-29 18:17:20 +0800

    test redeem CToken and add tokenB.

--- script/HW1.s.sol
+++ script/HW1.s.sol
@@ -7,15 +7,17 @@ import {CErc20Delegator} from "../contracts/CErc20Delegator.sol";
 import {CErc20Delegate} from "../contracts/CErc20Delegate.sol";
 import {WhitePaperInterestRateModel} from "../contracts/WhitePaperInterestRateModel.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {BearToken} from "../contracts/BearToken.sol";
+import {BearTokenA, BearTokenB} from "../contracts/BearToken.sol";
 import {ComptrollerG7} from "../contracts/ComptrollerG7.sol";
 import {SimplePriceOracle} from "../contracts/SimplePriceOracle.sol";
 import {Unitroller} from "../contracts/Unitroller.sol";
 
 contract HW1Script is Script {
-    ERC20 token;
+    ERC20 tokenA;
+    ERC20 tokenB;
     ComptrollerG7 comptroller;
-    CErc20Delegator cToken;
+    CErc20Delegator cTokenA;
+    CErc20Delegator cTokenB;
     CErc20Delegate impl;
     WhitePaperInterestRateModel model;
     Unitroller unitroller;
@@ -23,29 +25,46 @@ contract HW1Script is Script {
 
     function run() public {
         vm.startBroadcast();
+        _deploy(0x9ecE381e4d7173f9C1971Bf7797b616639EE9B42);
+        vm.stopBroadcast();
+    }
 
-        token = new BearToken();
+    function _deploy(address defaultSender) internal {
+        tokenA = new BearTokenA();
+        tokenB = new BearTokenB();
         impl = new CErc20Delegate();
         model = new WhitePaperInterestRateModel(0, 0);
         comptroller = new ComptrollerG7();
         unitroller = new Unitroller();
 
-        cToken = new CErc20Delegator(
-            address(token),
+        cTokenA = new CErc20Delegator(
+            address(tokenA),
             comptroller,
             model,
             1e18,
-            "Compound Bear Token",
-            "cBear",
+            "Compound Bear TokenA",
+            "cBearA",
             18,
-            payable(0x9ecE381e4d7173f9C1971Bf7797b616639EE9B42),
+            payable(defaultSender),
+            address(impl),
+            new bytes(0)
+        );
+
+        cTokenB = new CErc20Delegator(
+            address(tokenA),
+            comptroller,
+            model,
+            1e18,
+            "Compound Bear TokenB",
+            "cBearB",
+            18,
+            payable(defaultSender),
             address(impl),
             new bytes(0)
         );
 
         comptroller._setPriceOracle(oracle);
-        comptroller._supportMarket(CToken(address(cToken)));
-
-        vm.stopBroadcast();
+        comptroller._supportMarket(CToken(address(cTokenA)));
+        comptroller._supportMarket(CToken(address(cTokenB)));
     }
 }
--- test/HW2.t.sol
+++ test/HW2.t.sol
@@ -5,17 +5,28 @@ import "forge-std/Test.sol";
 import "../script/HW1.s.sol";
 
 contract HW2Test is Test, HW1Script {
+    address admin = makeAddr("Admin");
     address user1 = makeAddr("User1");
 
     function setUp() public {
-        super.run();
-        deal(address(token), user1, 100 ether);
+        vm.startPrank(admin);
+        _deploy(admin);
+        vm.stopPrank();
+        deal(address(tokenA), user1, 100 ether);
     }
 
     function testMintAndRedeem() public {
         vm.startPrank(user1);
-        token.approve(address(cToken), type(uint256).max);
-        cToken.mint(100 ether);
-        assertEq(cToken.balanceOf(user1), 100 ether);
+
+        tokenA.approve(address(cTokenA), type(uint256).max);
+        cTokenA.mint(100 ether);
+        assertEq(tokenA.balanceOf(user1), 0);
+        assertEq(cTokenA.balanceOf(user1), 100 ether);
+
+        cTokenA.redeem(100 ether);
+        assertEq(tokenA.balanceOf(user1), 100 ether);
+        assertEq(cTokenA.balanceOf(user1), 0);
+
+        vm.stopPrank();
     }
 }
