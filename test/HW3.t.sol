8ba8c2ec7c89df2f4e5a8db984def5984184c050
Edward <edward821220@gmail.com>
2023-11-30 18:50:25 +0800

    add borrow preset.

--- test/HW3.t.sol
+++ test/HW3.t.sol
@@ -27,13 +27,19 @@ contract HW3Test is Test {
     address user1 = makeAddr("User1");
     address user2 = makeAddr("User2");
     address user3 = makeAddr("User3");
-    uint256 initialBalance = 100 ether;
+    uint256 initialUSDC = 5000 * 1e6;
+    uint256 initialUNI = 5000 * 1e18;
 
     function setUp() public {
         // Fork Ethereum mainnet at block 17465000
         vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 17465000);
 
         vm.startPrank(admin);
+        impl = new CErc20Delegate();
+        model = new WhitePaperInterestRateModel(0, 0);
+        comptroller = new ComptrollerG7();
+        unitroller = new Unitroller();
+        oracle = new SimplePriceOracle();
         // cERC20 的 decimals 皆為 18，初始 exchangeRate 為 1:1
         cUSDC = new CErc20Delegator(
             address(USDC),
@@ -64,22 +70,39 @@ contract HW3Test is Test {
         // Liquidation incentive 設為 8%
         comptroller._setLiquidationIncentive(8e16);
         // 在 Oracle 中設定 USDC 的價格為 $1，UNI 的價格為 $5
+        comptroller._setPriceOracle(oracle);
         oracle.setUnderlyingPrice(CToken(address(cUSDC)), 1e30);
         oracle.setUnderlyingPrice(CToken(address(cUNI)), 5e18);
         // 設定 UNI 的 collateral factor 為 50%
         comptroller._setCollateralFactor(CToken(address(cUNI)), 5e17);
+        comptroller._supportMarket(CToken(address(cUSDC)));
+        comptroller._supportMarket(CToken(address(cUNI)));
         vm.stopPrank();
 
-        deal(address(USDC), user1, initialBalance);
-        deal(address(USDC), user2, initialBalance);
-        deal(address(USDC), user3, initialBalance);
-        deal(address(UNI), user1, initialBalance);
-        deal(address(UNI), user2, initialBalance);
-        deal(address(UNI), user3, initialBalance);
+        deal(address(USDC), user1, initialUSDC);
+        deal(address(USDC), user2, initialUSDC);
+        deal(address(USDC), user3, initialUSDC);
+        deal(address(UNI), user1, initialUNI);
+        deal(address(UNI), user2, initialUNI);
+        deal(address(UNI), user3, initialUNI);
     }
 
     function testHW3() public {
+        // User3 先提供流動性讓 User1 有東東可以借
+        vm.startPrank(user3);
+        USDC.approve(address(cUSDC), initialUSDC);
+        cUSDC.mint(initialUSDC);
+        vm.stopPrank();
+
         // * User1 使用 1000 顆 UNI 作為抵押品借出 2500 顆 USDC
+        vm.startPrank(user1);
+        UNI.approve(address(cUNI), initialUNI);
+        cUNI.mint(initialUNI);
+        address[] memory cTokens = new address[](1);
+        cTokens[0] = address(cUNI);
+        comptroller.enterMarkets(cTokens);
+        // cUSDC.borrow(2500 * 1e6);
+        vm.stopPrank();
         // * 將 UNI 價格改為 $4 使 User1 產生 Shortfall，並讓 User2 透過 AAVE 的 Flash loan 來借錢清算 User1
         // * 可以自行檢查清算 50% 後是不是大約可以賺 63 USDC
     }
